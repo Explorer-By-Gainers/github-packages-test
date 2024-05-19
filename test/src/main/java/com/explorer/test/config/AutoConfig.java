4d4da241f36681eaf4ea8b74e20a8fcde05d06b6
서지원 <sjs3326@naver.com>
2024-05-19 13:25:48 +0900

    [SERVER] chore: 자동 구성 만들기

---
+++ test/src/main/java/com/explorer/test/config/AutoConfig.java
@@ -0,0 +1,10 @@
+package com.explorer.test.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration
+@ComponentScan("com.explorer.test")
+public class AutoConfig {
+
+}
